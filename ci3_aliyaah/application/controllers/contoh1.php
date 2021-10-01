dad54da4e6c5045386e0828d34590f3548f06b15
pepentosee123 <aliyahuda33@gmail.com>
2021-10-01 12:33:25 +0300

    <ci3aliyah>

---
+++ ci3_aliyaah/application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 estends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya aliyah huda
+        saya tinggal di Bogor hobi bernang";
+    }
+
+
+}
+?>
